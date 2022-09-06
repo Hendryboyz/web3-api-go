338c89907a1c6069ea0fc91b60a7860bd08b5c32
Hendryboyz <hendry.js1247@gmail.com>
2022-09-06 09:30:01 +0800

    handle user input in controller

--- api/controllers/blocks_controllers.go
+++ api/controllers/blocks_controllers.go
@@ -1,13 +1,29 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type BlocksController struct{}
 
 func (c *BlocksController) GetBlockById(context *gin.Context) {
-
+	blockId := context.Param("id")
+	context.IndentedJSON(http.StatusOK, gin.H{"id": blockId})
 }
 
 func (c *BlocksController) GetBlocks(context *gin.Context) {
+	limit := strToInt64(context.Query("limit"))
+	context.IndentedJSON(http.StatusOK, gin.H{"limit": limit})
+}
 
+func strToInt64(s string) int64 {
+	number, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0
+	} else {
+		return number
+	}
 }
--- api/controllers/transactions_controllers.go
+++ api/controllers/transactions_controllers.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 type TransactionsController struct{}
 
 func (c *TransactionsController) GetTransactionByHash(context *gin.Context) {
-
+	hash := context.Param("txHash")
+	context.IndentedJSON(http.StatusOK, gin.H{"txHash": hash})
 }
